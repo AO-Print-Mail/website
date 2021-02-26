1ffc47257f777bc9f92c0520fc96b88f978b8a0b
slightly-askew <oli@oaskew.co.uk>
2021-02-26 13:53:01 +1100

    resolve merge conflict

--- src/components/landing-page-quote-form/src/step2.tsx
+++ src/components/landing-page-quote-form/src/step2.tsx
@@ -59,11 +59,7 @@ export const Step2: React.FC<Step2Props> = ({ changeStep }) => {
     addressDataReady,
     additionalInformation,
     services,
-<<<<<<< HEAD
-    //@ts-ignore-error
-=======
     //@ts-ignore
->>>>>>> develop
   } = state.formData?.directMailForm
 
   const requiresArtwork = services === 'Print and mail'
