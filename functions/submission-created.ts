9da1d822a76c504b4d16a4fe791e3271ccdb298a
Oliver Askew <oli@oaskew.co.uk>
2021-03-16 08:46:25 +1100

    wip function

--- functions/submission-created.ts
+++ functions/submission-created.ts
@@ -25,3 +25,16 @@ const contextNameLookup = {
   pageUri: 'pageUri',
   submittedAt: 'submittedAt',
 }
+import { APIGatewayEvent, Context } from 'aws-lambda'
+
+export async function handler(event: APIGatewayEvent, context: Context) {
+  const { msg } = event.queryStringParameters
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ msg }),
+  }
+}
