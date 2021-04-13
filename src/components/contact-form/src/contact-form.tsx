024e0e4e2513b1f8d2563dc72e25cb2fe973828a
slightly-askew <oli@oaskew.co.uk>
2021-04-13 15:19:57 +1000

    yup schema

--- src/components/contact-form/src/contact-form.tsx
+++ src/components/contact-form/src/contact-form.tsx
@@ -1,5 +1,24 @@
 import { styled, Card } from '@theme'
+import * as yup from 'yup'
+
 interface ContactFormProps {}
+
+const schema = yup.object().shape({
+  firstName: yup.string().required('Please enter a first name'),
+  lastName: yup.string(),
+  companyName: yup.string(),
+  email: yup
+    .string()
+    .email('Please provide a valid email address')
+    .required('We need an email to send your quote!'),
+  phone: yup
+    .string()
+    .required(`Please enter a telephone number`)
+    .min(9, 'Please enter a full telephone number')
+    .max(14, 'The telephone number you entered seems too long.'),
+  country: yup.string(),
+  message: yup.string(),
+})
 
 const Background = styled(Card, {
   height: '768px',
