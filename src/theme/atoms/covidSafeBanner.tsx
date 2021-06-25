4221b78edc4cc582669577dbb166bd9fbafa9670
slightly-askew <oli@oaskew.co.uk>
2021-06-25 10:37:50 +1000

    Add link to covid badge

--- src/theme/atoms/covidSafeBanner.tsx
+++ src/theme/atoms/covidSafeBanner.tsx
@@ -1,6 +1,7 @@
 import { CSS, styled } from '@theme/stitches.config'
 import { Paragraph } from '@theme/typography/text'
 import { CovidSafe } from '../icons'
+import Link from 'next/link'
 
 export interface CovidSafeBannerProps {
   css?: CSS
@@ -17,6 +18,19 @@ const Background = styled('div', {
   '@m': { px: '$4', py: '$2' },
   '@l': { px: '$5', py: '$2' },
 })
+
+const A = styled('a', {
+  textDecoration: 'none',
+  color: 'inherit',
+  [`${Background}:hover &`]: {
+    color: 'white',
+  },
+  '&:hover,&:focus': {
+    textDecoration: 'underline',
+    color: 'white',
+  },
+})
+
 export const CovidSafeBanner: React.FC<CovidSafeBannerProps> = (props) => {
   return (
     <Background {...props}>
@@ -25,7 +39,10 @@ export const CovidSafeBanner: React.FC<CovidSafeBannerProps> = (props) => {
         size="xs"
         css={{ ml: '$4', color: '$LA80', mt: '0', flex: '0 1 100%' }}
       >
-        A&amp;O is fully open for business and operating with a COVID safe plan.
+        A&amp;O is fully open for business and operating with a&nbsp;
+        <Link passHref href="/legal/covid-response">
+          <A>COVID safe plan.</A>
+        </Link>
       </Paragraph>
     </Background>
   )
